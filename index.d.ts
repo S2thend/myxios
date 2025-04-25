6b98cb8becfaee4d51f94562e6a024890e3dc5f9
borui <borui_cai@qq.com>
2025-04-25 10:05:14 +0800

    fixs: :wrench: fix ts types

--- index.d.ts
+++ index.d.ts
@@ -4,13 +4,13 @@ export type response =
     | Object
     | Array<String | Number | Object>;
 
-declare function response_interceptor(res: response): Promise<response>;
+declare function response_interceptor(res: response): Promise<response>|Promise<Array<response>>;
 
 export class Myxios {
     constructor(response_interceptors?: Array<response_interceptor>);
     response_interceptors: Array<response_interceptor>;
-    request(url: String, options?: Object): Promise<response>;
-    requestOneTimeIntercepts(url: String, options?: Object, interceptors?: Array<response_interceptor>): Promise<response>;
+    request(url: String, options?: Object): Promise<Array<response>>;
+    requestOneTimeIntercepts(url: String, options?: Object, interceptors?: Array<response_interceptor>): Promise<Array<response>>;
     addResponseInterceptor(interceptor: response_interceptor): void;
     removeResponseInterceptor(interceptor: response_interceptor): void;
 }
@@ -18,6 +18,6 @@ export class Myxios {
 
 export function createInstance(response_interceptors?: Array<response_interceptor>): Myxios;
 
-declare function dispatchRequest(url: String, options?: Object): Promise<response>;
+declare function dispatchRequest(request: Promise<response>|Promise<Array<response>>, response_interceptors?: Array<response_interceptor>|Array<response_interceptor>): Promise<Array<response>>;
 
-declare function paramCarryPromise(param: response, promise: Promise<response>): Promise<response>;
+declare function paramCarryPromise(param: response, promise: Promise<response>|Promise<Array<response>>,): Promise<Array<response>>;
